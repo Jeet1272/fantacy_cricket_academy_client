b3d672c98a2f38db0b5f607728a7cae62a85c0d5
Jeet1272 <bishwajeetsarkarbd@gmail.com>
2022-10-25 23:07:38 +0600

    Login done

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,21 +6,40 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <Form className='w-50 mx-auto border border-success my-5 p-3'>
-            <Form.Group className="my-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+        <div>
+            <div>
+                <Form className='bg-light w-50 mx-auto border border-success mt-5 px-3'>
+                    <Form.Group className="my-2" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
 
-            <Form.Group className="my-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-            <p className='my-3'>New in this website then Please <Link to='/register'>Register</Link></p>
-        </Form>
+                    <Form.Group className="my-2" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Log In
+                    </Button>
+                    <p><small>
+                        Don’t have an account yet? <Link to='/register'>Sign Up</Link>
+                    </small></p>
+                </Form>
+            </div>
+            <div className="d-grid gap-3 w-25 mx-auto mb-3">
+                <p className='ms-4 mt-3'>Please log in with one of the following:</p>
+                <Button variant="dark" size="md">
+                    Github
+                </Button>
+                <Button variant="warning" size="md">
+                    Google
+                </Button>
+                <Button variant="secondary" size="md">
+                    <Link className='text-white text-decoration-none' to='/register'>Email</Link>
+                </Button>
+
+            </div>
+        </div>
     );
 };
 
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -3,55 +3,61 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <Form className='w-50 mx-auto border border-success m-5 p-3'>
+        <div>
+            <Form className='bg-light w-50 mx-auto border border-success m-5 p-3'>
 
-            <Form.Group controlId="formGridName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" name='name' placeholder="Enter name" />
-            </Form.Group>
-            <Form.Group controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" name='email' placeholder="Enter email" />
-            </Form.Group>
-
-            <Form.Group controlId="formGridPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" name='password' placeholder="Password" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formGridPhotoURL">
-                <Form.Label>Photo URL</Form.Label>
-                <Form.Control type='text' name='photoURL' placeholder="Photo URL" />
-            </Form.Group>
-
-
-            <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridCity">
-                    <Form.Label>City</Form.Label>
-                    <Form.Control />
+                <Form.Group controlId="formGridName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Enter name" />
+                </Form.Group>
+                <Form.Group controlId="formGridEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridState">
-                    <Form.Label>State</Form.Label>
-                    <Form.Select defaultValue="Choose...">
-                        <option>Choose...</option>
-                        <option>...</option>
-                    </Form.Select>
+                <Form.Group controlId="formGridPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" />
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridZip">
-                    <Form.Label>Zip</Form.Label>
-                    <Form.Control />
+                <Form.Group className="mb-3" controlId="formGridPhotoURL">
+                    <Form.Label>Photo URL</Form.Label>
+                    <Form.Control type='text' name='photoURL' placeholder="Photo URL" />
                 </Form.Group>
-            </Row>
 
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridCity">
+                        <Form.Label>City</Form.Label>
+                        <Form.Control />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridState">
+                        <Form.Label>State</Form.Label>
+                        <Form.Select defaultValue="Choose...">
+                            <option>Choose...</option>
+                            <option>...</option>
+                        </Form.Select>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridZip">
+                        <Form.Label>Zip</Form.Label>
+                        <Form.Control />
+                    </Form.Group>
+                </Row>
+
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+                <p className='mt-2'>
+                    <small>Already have an account?   <Link to='/login'>Log In</Link> </small>
+                </p>
+            </Form>
+        </div>
     );
 };
 
