98b6ca894bed3cc0ecc6a85b835f1858de94aebb
Jeet1272 <bishwajeetsarkarbd@gmail.com>
2022-10-27 00:44:16 +0600

    Course details done

--- src/pages/Courses/Courses/CourseDetails.js
+++ src/pages/Courses/Courses/CourseDetails.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 
 const CourseDetails = () => {
     const course = useLoaderData();
     console.log(course)
+    const { name, picture, price, auther, views, details } = course
     return (
-        <div>
-
+        <div className='my-5'>
+            <Card style={{ width: '40rem', margin: '0px auto' }}>
+                <Card.Img variant="top" src={picture} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        {details}
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item className='text-success'>By: {auther}</ListGroup.Item>
+                    <ListGroup.Item>{price}</ListGroup.Item>
+                    <ListGroup.Item>Views: {views}</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <Button variant="primary"><Link className='text-white' to='/checkout' >Get premium access</Link></Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
